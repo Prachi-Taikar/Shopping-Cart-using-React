938a52f9729811d476dc88cc98950628f7a09b7c
Prachi <prachitaikar1994@gmail.com>
2022-02-18 01:22:44 +0530

    rendering productList and creating Product component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
 
 function App() {
+
+  const productList =[
+    {
+      price : 99999,
+      name : "IPhone",
+      quantity: 0,
+    },
+    {
+      price : 9999,
+      name : "Redmi",
+      quantity: 0,
+    }
+  ]
+
   return (
     <>
     <Navbar/>
+    
+    <main className= "container mt-5">
+    <ProductList productList={productList} />
+    </main>
+
+    {/* <Footer/> */}
     </>
   );
 }
